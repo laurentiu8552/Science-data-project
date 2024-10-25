21199423352346c37589fe4b6521f294b330e763
laurentiu8552 <babilevlaurentiu@gmail.com>
2024-10-25 11:26:04 +0000

    derji

---
+++ credentials.R
@@ -0,0 +1,16 @@
+cred_psql_docker <- list(dbname = "postgres",
+                         host   = "postgres",
+                         user      = 'postgres',
+                         pass      = 'you-own-password-for-postgres',
+                         port = 5432)
+
+cred_psql_150 <- list(dbname = "databasename-from-brightspace",
+                         host   = "159.223.30.236",
+                         user      = 'username-from-brightspace',
+                         pass      = 'password-from-brightspace',
+                         port = 6432)
+cred_psql_150_dvd <- list(dbname = "dvdrental",
+                          host   = "159.223.30.236",
+                          user      = 'username-from-brightspace',
+                          pass      = 'password-from-brightspace',
+                          port = 6432)
